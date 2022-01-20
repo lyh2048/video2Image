3acb348574b968e45860baf98a122a73b3ec1e1d
lyh <1046818625@qq.com>
2022-01-20 10:18:36 +0800

    项目重构

--- README.md
+++ README.md
@@ -1,41 +1,72 @@
 # Video2Image
 
-## 使用说明
+嗨 ; )  <img src="assets/hi.gif" width="40">   
 
-1、安装依赖，需要安装`opencv-python`
+这是一个基于`Python`的视频转图片小工具，实现了指定间隔的图片提取，但仍存在许多不足与bug。如果发现错误，还请在`Issues`中指出，欢迎`Fork`和`Pull requests`改善代码，谢谢！
 
-> `numpy==1.21.3`
-> `opencv-python==4.5.4.58`
+[Installation](#Installation) | [Usage](#Usage) | [Example](#Example)
+
+ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʕ•̫͡•ʔ-̫͡-ʕ•͓͡•ʔ-̫͡-ʔ
+
+## Installation
+
+该项目使用`Python`开发，运行之前，请确保已经安装好`Python`
+
+[![](assets/python-logo.png)](https://www.python.org/)
+
+[Welcome to Python.org](https://www.python.org/)
+
+1 克隆仓库
 
 ```bash
-pip install -r .\requirements.txt
+git clone https://github.com/lyh2048/video2Image.git
+```
+
+2 进入目录
+
+```bash
+cd ./video2Image
+```
+
+3 安装依赖
+
+```bash
+pip install -r ./requirements.txt
+```
+
+Then enjoy the video2Image ~
+
+## Usage
+
+```bash
+python ./main.py -input ./input/test.mp4
 ```
 
 
 
-2、运行程序
+> usage: main.py [-h] -input INPUT [-output OUTPUT] [-interval INTERVAL]
+>
+> Video2Image
+>
+> optional arguments:
+>
+>   -h, --help          show this help message and exit
+>
+>   -input INPUT        输入视频的路径
+>
+>   -output OUTPUT      保存位置
+>
+>   -interval INTERVAL  指定间隔
+
+
+
+## Example
 
 ```bash
-python .\main.py -i ./input/test.mp4 -m 2 -n 2 -o ./output/test22.png
+python ./main.py -input ./input/test.mp4 -interval 10
 ```
 
 
 
-## 示例
-
-```bash
-python .\main.py -i ./input/test.mp4 -m 2 -n 2 -o ./output/test22.png
-```
-
->参数说明
->
->-i  	指定输入视频的路径
->
->-o 	指定输出图片的路径
->
->-m	指定行数
->
->-n	 指定列数
-
-![test22](README.assets/test22.png)
+![image-20220120100502507](assets/image-20220120100502507.png)
 
--- main.py
+++ main.py
@@ -1,61 +1,53 @@
-import random
-import cv2
-import os
-import numpy as np
 import argparse
+from fileinput import filename
+import os
+import cv2
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Video2Image")
-    parser.add_argument("-i", required=True)
-    parser.add_argument("-m", required=True)
-    parser.add_argument("-n", required=True)
-    parser.add_argument("-o", required=True)
-    args = parser.parse_args()
-    input_path = args.i
-    output_path = args.o
-    m = int(args.m)
-    n = int(args.n)
-    if m <= 0 or n <= 0:
-        exit(0)
-    if not os.path.exists(input_path):
-        print("文件{}不存在！".format(input_path))
-        exit(0)
-    frame_list = []
-    cap = cv2.VideoCapture(input_path)
+'''
+Video2Image
+'''
+
+
+def run(video_path, ouput, interval):
+    # 判断视频文件是否存在
+    if not os.path.exists(video_path):
+        print(f'文件 {video_path} 不存在')
+        exit(-1)
+    # 判断输出目录是否存在
+    if not os.path.exists(output):
+        os.makedirs(output)
+    # 提取视频名称
+    video_name = video_path.split('/')[-1]
+    # 读入视频
+    cap = cv2.VideoCapture(video_path)
+    i = 0
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
-        frame_list.append(frame)
-        print("loading...")
+        if i % interval == 0:
+            filename = f'{video_name.split(".")[0]}_{str(i)}.png'
+            save_image(output, filename, frame)
+        i += 1
     cap.release()
-    total = m * n
-    frame_list_length = len(frame_list)
-    if total >= frame_list_length:
-        total = frame_list_length
-    # 如果能整除则等间隔取，否则就随机取
-    result_list = []
-    k = frame_list_length % total
-    if k == 0:
-        sep = frame_list_length / total
-        for index, frame in enumerate(frame_list):
-            if index % sep == 0:
-                result_list.append(frame)
-    else:
-        index_list = random.sample(range(0, frame_list_length), total)
-        # 升序排列
-        index_list.sort()
-        for index in index_list:
-            result_list.append(frame_list[index])
-    assert len(result_list) > 0
-    width = result_list[0].shape[1]
-    height = result_list[0].shape[0]
-    target_img = np.zeros((height*m, width*n, 3), np.uint8)
-    for index, frame in enumerate(result_list):
-        i = index // n
-        j = index % n
-        print(i, j)
-        target_img[i*height:height+i*height, j*width:width+j*width, :] = frame
-    cv2.imwrite(output_path, target_img)
-    print(output_path)
+
+
+def save_image(output, filename, img):
+    cv2.imwrite(os.path.join(output, filename), img)
+    print(f'write:{os.path.join(output, filename)}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Video2Image')
+    parser.add_argument('-input', type=str, required=True, help='输入视频的路径')
+    parser.add_argument('-output', type=str, default='./output', help='保存位置')
+    parser.add_argument('-interval', type=int, default=5, help='指定间隔')
+    args = parser.parse_args()
+    video_path = args.input
+    output = args.output
+    interval= args.interval
+    print('---- Video2Image ----')
+    run(video_path, output, interval)
+    print('处理完成, 输出路径:', output)
+    print('---- Video2Image ----')
